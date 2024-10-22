879919aa4cc0cb3941f871fc98cc58c0fac1c68b
ilieortizcunha <ilieortizcunha@gmail.com>
2024-10-21 22:39:34 -0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const rotaLivro = require("./rotas/rotasLivro")
+const cors  = require("cors")
 
 const app = express()
-
 app.use(express.json())
+app.use(cors({origin: "*"}))
 
 app.use('/livro', rotaLivro)
 
